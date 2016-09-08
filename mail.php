1f8097cfd815b9c48e83dee598976e81a1deb450
Adrian1690 <j.adrian.1690@gmail.com>
2016-09-08 16:30:03 -0500

    Mail

--- mail.php
+++ mail.php
@@ -21,18 +21,19 @@ exit();*/
 
 
 
-$to = 'j.adrian.1690@gmail.com'; // Cambiar a Cecília.
+$to = 'bob@tutamanta.pe'; // Cambiar a Cecília.
+//$to = 'adrian@e-quipu.pe'; // Cambiar a Cecília.
 $subject = 'MENSAJE 3G';
 
-$message .=  "Este mensaje es enviado desde el formularo de la web Tres Generacioness.\r\n\r\n";
+$message .=  "Este mensaje es enviado desde el formularo de la web Tres Generaciones.\r\n\r\n";
 $message .=  "Nombre : " . $nombre. "\r\n";
 $message .=  "Telefono : " . $telefono. "\r\n";
 $message .=  "Mensaje : " . $mensaje. "\r\n";
 
 //$headers  = 'MIME-Version: 1.0' . "\r\n";
 //$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= "From: $name < $correo >" . "\r\n";
-//$headers .= "Reply-To: adrian@e-quipu.pe" . "\r\n";
+$headers = "From: $name < $correo >" . "\r\n";
+//$headers .= "Reply-To: j.adrian.1690@gmail.com" . "\r\n";
 
 //$headers = "From: $name < $correo >" . "\r\n" .
     //'Reply-To: webmaster@example.com' . "\r\n" .
